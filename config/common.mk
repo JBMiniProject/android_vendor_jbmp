36e2fbe479b6ff2ebe028e26158f2283b07af5b9
Daveee10 <daver16@msn.com>
2012-07-24 21:50:47 +0200

    Initial commit for Jelly bean

---
+++ config/common.mk
@@ -0,0 +1,103 @@
+ifneq ($(TARGET_BOOTANIMATION_NAME),)
+    PRODUCT_COPY_FILES += \
+        vendor/jbmp/prebuilt/common/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip:system/media/bootanimation.zip
+endif
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    keyguard.no_require_sim=true \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.com.android.dataroaming=false
+
+# Copy over the changelog to the device
+PRODUCT_COPY_FILES += \
+    vendor/cm/CHANGELOG.mkdn:system/etc/CHANGELOG-CM.txt
+
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
+    vendor/cm/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions \
+    vendor/cm/prebuilt/common/bin/50-cm.sh:system/addon.d/50-cm.sh
+
+# init.d support
+PRODUCT_COPY_FILES += \
+    vendor/jbmp/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
+    vendor/cm/prebuilt/common/bin/sysinit:system/bin/sysinit
+
+# userinit support
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
+
+# Compcache/Zram support
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/etc/init.local.rc:system/etc/init.local.rc \
+    vendor/cm/prebuilt/common/bin/compcache:system/bin/compcache \
+    vendor/cm/prebuilt/common/bin/handle_compcache:system/bin/handle_compcache
+
+# Nam configuration script
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/bin/modelid_cfg.sh:system/bin/modelid_cfg.sh
+
+PRODUCT_COPY_FILES +=  \
+    vendor/jbmp/proprietary/Term.apk:system/app/Term.apk \
+    vendor/jbmp/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so \
+    vendor/jbmp/prebuilt/common/apps/Superuser.apk:system/app/Superuser.apk
+
+# Enable SIP+VoIP on all targets
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# This is CM!
+PRODUCT_COPY_FILES += \
+    vendor/cm/config/permissions/com.cyanogenmod.android.xml:system/etc/permissions/com.cyanogenmod.android.xml
+
+# Don't export PS1 in /system/etc/mkshrc.
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/etc/mkshrc:system/etc/mkshrc
+
+# T-Mobile theme engine
+include vendor/cm/config/themes_common.mk
+
+# Required CM packages
+PRODUCT_PACKAGES += \
+    LatinIME \
+    Superuser \
+    Superuser.apk \
+    su
+
+# Optional CM packages
+PRODUCT_PACKAGES += \
+    VoiceDialer \
+    SoundRecorder \
+    LiveWallpapers \
+    LiveWallpapersPicker
+
+# Custom CM packages
+PRODUCT_PACKAGES += \
+    Trebuchet \
+    DSPManager \
+    libcyanogen-dsp \
+    Apollo
+
+# Extra tools in CM
+PRODUCT_PACKAGES += \
+    openvpn \
+    e2fsck \
+    mke2fs \
+    tune2fs
+
+PRODUCT_VERSION_MAJOR = 10
+PRODUCT_VERSION_MINOR = 0
+PRODUCT_BUILDTYPE := ALPHA
+
+JBMP_VERSION := JBMP-v$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)_$(PRODUCT_BUILDTYPE)-$(PRODUCT_VERSION_DEVICE_SPECIFIC)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+  ro.cm.version=$(JBMP_VERSION) \
+  ro.modversion=$(JBMP_VERSION)
---
+++ config/common_full_phone.mk
@@ -0,0 +1,25 @@
+# Inherit common CM stuff
+$(call inherit-product, vendor/jbmp/config/common.mk)
+
+# Bring in all audio files
+include frameworks/base/data/sounds/NewAudio.mk
+
+# Extra Ringtones
+include frameworks/base/data/sounds/AudioPackageNewWave.mk
+
+# Include CM audio files
+include vendor/cm/config/cm_audio.mk
+
+# Default ringtone
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.ringtone=CyanTone.ogg \
+    ro.config.notification_sound=CyanMessage.ogg \
+    ro.config.alarm_alert=CyanAlarm.ogg
+
+PRODUCT_PACKAGES += \
+  Mms
+
+ifeq ($(TARGET_BOOTANIMATION_NAME),)
+    PRODUCT_COPY_FILES += \
+        vendor/jbmp/prebuilt/common/bootanimation/vertical-320x480.zip:system/media/bootanimation.zip
+endif
